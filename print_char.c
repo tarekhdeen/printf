52ac1b29d70cd4476dde1da7e49ae3de881146e0
mohamedhossam2377 <m.hossam2377@gmail.com>
2023-10-17 15:34:06 -0700

    print char & string

--- print_char.c
+++ print_char.c
@@ -2,14 +2,32 @@
 #include <stdarg.h>
 #include "main.h"
 
+#define BUFFER_SIZE 1024
+
 /**
  * print_char - writes the character c to stdout
  * @c: The character to print
+ * @buffer: char
+ * @buffer_index: int
+ * @count: int
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int print_char(char c)
+int print_char(char c, char *buffer, int *buffer_index, int *count)
 {
-		return (write(1, &c, 1));
+	if (buffer == NULL || buffer_index == NULL || count == NULL)
+	{
+		return (-1);
+	}
+
+	if (*buffer_index == BUFFER_SIZE - 1)
+	{
+		write(1, buffer, BUFFER_SIZE);
+		*buffer_index = 0;
+	}
+	buffer[(*buffer_index)++] = c;
+	(*count)++;
+
+	return (1);
 }
--- print_string.c
+++ print_string.c
@@ -3,24 +3,38 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdarg.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 1024
 
 /**
  * print_string - print string
  * @s: const char
+ * @buffer: char
+ * @buffer_index: int
+ * @count: int
  *
  * Return: 0
  */
-int print_string(const char *s)
+int print_string(const char *s, char *buffer, int *buffer_index, int *count)
 {
-	if (s == NULL)
+	int len;
+
+	if (s == NULL || buffer == NULL || buffer_index == NULL)
 	{
 		return (-1);
 	}
-	else
-	{
-		int len = strlen(s);
 
-		write(1, s, len);
+	len = strlen(s);
+
+	if (*buffer_index + len >= BUFFER_SIZE)
+	{
+		write(1, buffer, *buffer_index);
+		*buffer_index = 0;
 	}
+	strcpy(buffer + *buffer_index, s);
+	*buffer_index += len;
+	(*count) += len;
+
 	return (0);
 }
