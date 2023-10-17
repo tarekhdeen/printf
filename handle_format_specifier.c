5eb305e2314f7618abb7ce0798da1e44b9fd4f5b
mohamedhossam2377 <m.hossam2377@gmail.com>
2023-10-17 16:09:28 -0700

    handle

--- handle_format_specifier.c
+++ handle_format_specifier.c
@@ -1,103 +1,44 @@
-#include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
 #include <string.h>
 #include "main.h"
 
-/**
- * handle_character - handle c
- * @args: args
- * @count: integer
- *
- * Return: 0
- */
-int handle_character(va_list args, int *count)
-{
-	char c = va_arg(args, int);
-
-	print_char(c);
-	(*count)++;
-	return (0);
-}
-
-/**
- * handle_string - handle s
- * @args: args
- * @count: integer
- *
- * Return: 0
- */
-int handle_string(va_list args, int *count)
-{
-	const char *s = va_arg(args, const char *);
-	int result = print_string(s);
-
-	if (result == -1)
-	{
-		return (-1);
-	}
-	(*count) += strlen(s);
-	return (0);
-}
-
-/**
- * handle_percent - handle %
- * @count: integer
- *
- * Return: 0
- */
-int handle_percent(int *count)
-{
-	print_char('%');
-	(*count)++;
-	return (0);
-}
-
-/**
- * handle_integer - handle integer
- * @args: args
- * @count: intger
- *
- * Return: 0
- */
-int handle_integer(va_list args, int *count)
-{
-	char num_str[12];
-	int num, result;
-
-	num = va_arg(args, int);
-	sprintf(num_str, "%d", num);
-	result = print_string(num_str);
-
-	if (result == -1)
-	{
-		return (-1);
-	}
-	(*count) += strlen(num_str);
-	return (0);
-}
+#define BUFFER_SIZE 1024
 
 /**
  * handle_format_specifier - handle format specifier
  * @format: const char
  * @args: args
  * @count: integer
+ * @buffer: char
+ * @buffer_index: int
  *
  * Return: 0
  */
-int handle_format_specifier(const char *format, va_list args, int *count)
+int handle_format_specifier(
+const char *format, va_list args, char *buffer, int *buffer_index, int *count)
 {
 	switch (*format)
 	{
 		case 'c':
-			return (handle_character(args, count));
+			return (handle_character(args, buffer, buffer_index, count));
 		case 's':
-			return (handle_string(args, count));
+			return (handle_string(args, buffer, buffer_index, count));
 		case '%':
-			return (handle_percent(count));
+			return (handle_percent(buffer, buffer_index, count));
 		case 'd':
 		case 'i':
-			return (handle_integer(args, count));
+			return (handle_integer(args, buffer, buffer_index, count));
+		case 'b':
+			return (handle_binary(args, buffer, buffer_index, count));
+		case 'u':
+			return (handle_unsigned_int(args, buffer, buffer_index, count));
+		case 'o':
+			return (handle_octal(args, buffer, buffer_index, count));
+		case 'x':
+			return (handle_hex_lower(args, buffer, buffer_index, count));
+		case 'X':
+			return (handle_hex_upper(args, buffer, buffer_index, count));
 		default:
 			return (-1);
 	}
